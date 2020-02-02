0b1707a2c50c838386b220263095c2786ada7bea
Lucian Amos <locamos@gmail.com>
2020-02-02 11:17:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,6 +14,17 @@ const codes = [
 function init() {
   document.body.addEventListener("keydown", event => {
     let index = 0;
+    const key = event.key;
     
+    if (key === codes[index]) {
+      index ++;
+      
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   });
 }
